75dc784c56f6722f97d6d4e7dcfe2dc40f4e4ede
m82425342201 <m82425342201@gmail.com>
2022-04-25 03:46:56 +0000

    ...

--- init.sh
+++ init.sh
@@ -3,4 +3,3 @@ cd /workspace
 sudo apt update
 sudo apt install openssh-server rsync sshpass -y
 sudo /etc/init.d/ssh start
-git clone https://gitlab.com/m82425342201/Apps
---
+++ install-apps.sh
@@ -0,0 +1,1 @@
+git clone https://gitlab.com/m82425342201/Apps
