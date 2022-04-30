f8bb9daa9b6be1c4601d213f6751ddcb7bd3a4da
m82425342201 <m82425342201@gmail.com>
2022-04-30 17:38:43 +0900

    Update install-elixir.sh

--- install-elixir.sh
+++ install-elixir.sh
@@ -2,4 +2,5 @@
 wget https://packages.erlang-solutions.com/erlang-solutions_2.0_all.deb && sudo dpkg -i erlang-solutions_2.0_all.deb
 \rm erlang-solutions_2.0_all.deb
 sudo apt update
+sudo apt install keyboard-configuration
 sudo apt install esl-erlang elixir
