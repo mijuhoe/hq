277c712cc1f1dc303a9337455acb202f9f4ba10a
m82425342201 <m82425342201@gmail.com>
2022-06-28 23:46:33 +0900

    Create git-clone-all.sh

---
+++ git-clone-all.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/camunda/camunda-bpm-platform.git
+git clone https://github.com/camunda/camunda-modeler.git
+git clone https://github.com/valyala/fasthttp.git
+git clone https://gitlab.com/jayeonsoo/http2cbench
+git clone https://gitlab.com/jayeonsoo/http2cbench_otp.git
+git clone https://gitlab.com/jayeonsoo/http2cbenchotp
+git clone https://github.com/apache/jmeter.git
+git clone https://github.com/locustio/locust.git
+git clone https://github.com/netty/netty.git
+git clone https://github.com/naver/ngrinder.git
+git clone https://github.com/microsoft/playwright.git
+git clone https://github.com/puppeteer/puppeteer.git
+git clone https://github.com/addyosmani/puppeteer-webperf.git
+git clone https://github.com/camunda/zeebe.git
