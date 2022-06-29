08bfe5552b23f51b6b2036612724c508d7ccdad0
m82425342201 <m82425342201@gmail.com>
2022-06-30 08:26:25 +0900

    Update git-clone-all.sh

--- git-clone-all.sh
+++ git-clone-all.sh
@@ -1,9 +1,6 @@
 git clone https://github.com/camunda/camunda-bpm-platform.git
 git clone https://github.com/camunda/camunda-modeler.git
 git clone https://github.com/valyala/fasthttp.git
-git clone https://gitlab.com/jayeonsoo/http2cbench
-git clone https://gitlab.com/jayeonsoo/http2cbench_otp.git
-git clone https://gitlab.com/jayeonsoo/http2cbenchotp
 git clone https://github.com/apache/jmeter.git
 git clone https://github.com/locustio/locust.git
 git clone https://github.com/netty/netty.git
