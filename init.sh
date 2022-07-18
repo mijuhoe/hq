d44e9d46a17f38739c466e4558689c7e6e9079ef
m82425342201 <m82425342201@gmail.com>
2022-07-18 10:00:31 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ sudo apt update
 sudo apt install openssh-server rsync sshpass -y
 echo gitpod:Behd903gh|sudo chpasswd
 sudo /etc/init.d/ssh start
-sh /workspace/pkgs2/install.sh
+#sh /workspace/pkgs2/install.sh
