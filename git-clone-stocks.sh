600b9995c122de3a988417872a600825f39bd545
m82425342201 <m82425342201@gmail.com>
2022-08-01 12:52:25 +0900

    Create git-clone-stocks.sh

---
+++ git-clone-stocks.sh
@@ -0,0 +1,5 @@
+git clone https://gitlab.com/jayeonsoo/ex-playwright-1
+git clone https://gitlab.com/m8228308850/alphasquare_downloader
+git clone https://gitlab.com/m8228308850/stock_naver
+git clone https://gitlab.com/m8228308850/stock_daum
+git clone https://gitlab.com/m8228308850/stock_krx
