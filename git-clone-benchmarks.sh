d7ada4b59614e213b685636de948c05bc23899ed
Your Name <you@example.com>
2022-08-08 03:52:19 +0000

    ...

--- git-clone-benchmarks.sh
+++ git-clone-benchmarks.sh
@@ -1,6 +1,6 @@
-https://github.com/the-benchmarker/web-frameworks.git
-https://github.com/TechEmpower/FrameworkBenchmarks.git
-#https://github.com/mingrammer/python-web-framework-stars.git
-#https://github.com/bttger/web-framework-benchmarks.git
-#https://github.com/rw3iss/web-framework-benchmarks.git
-#https://github.com/smilingleo/web-framework-benchmark.git
+git clone https://github.com/the-benchmarker/web-frameworks.git
+git clone https://github.com/TechEmpower/FrameworkBenchmarks.git
+# git clone https://github.com/mingrammer/python-web-framework-stars.git
+# git clone https://github.com/bttger/web-framework-benchmarks.git
+# git clone https://github.com/rw3iss/web-framework-benchmarks.git
+# git clone https://github.com/smilingleo/web-framework-benchmark.git
