a51a767dc3d87942a963e7ebe6b81b64c1cd2785
Your Name <you@example.com>
2022-09-06 12:53:36 +0000

    ...

--- init-2.sh
+++ init-2.sh
@@ -1,5 +1,6 @@
+cp .vscode/settings.json.1 .vscode/settings.json
 sh init.sh
+sh download-otp-23.sh
 sh ../otp-23/install.sh
 sh install-hamler.sh
 sh install-purescript.sh
-cp .vscode/settings.json.1 .vscode/settings.json
