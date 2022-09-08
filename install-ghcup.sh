50bed3cb7a5e03cc59bdb1bd509518d69846ab90
Your Name <you@example.com>
2022-09-08 03:13:26 +0000

    ...

--- install-ghcup.sh
+++ install-ghcup.sh
@@ -7,10 +7,10 @@ done
 curl --proto 'https' -tlsv1.2 -sSf https://downloads.haskell.org/~ghcup/x86_64-linux-ghcup > ./ghcup
 chmod 755 ./ghcup
 sudo mv ./ghcup /usr/local/bin
-ghcup install ghc 8.6.4
+ghcup install ghc 8.10.7
 ghcup install cabal 3.8.1.0
 ghcup install stack
-ghcup set ghc 8.6.4
+ghcup set ghc 8.10.7
 ghcup set cabal 3.8.1.0
 for f in stack cabal ghc ghci; do
   sudo rm -f /usr/local/bin/$f
