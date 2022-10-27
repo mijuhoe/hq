6766de19d62f58c3633ac35e0fe06aa019178607
Your Name <you@example.com>
2022-10-27 07:09:35 +0000

    ...

--- git-clone-stock.sh
+++ git-clone-stock.sh
@@ -1,5 +1,5 @@
-mkdir -p /workspace/stocks
-cd /workspace/stocks
+mkdir -p /workspace/stock
+cd /workspace/stock
 git clone ssh://git@gitlab.com/m8228308850/stock_naver
 git clone ssh://git@gitlab.com/m8228308850/stock_daum
 git clone ssh://git@gitlab.com/m8228308850/stock_krx
