f7a2108b59fc00a2de5d0d8009cf7a6ed4b2d68e
Your Name <you@example.com>
2022-11-04 02:51:33 +0000

    ...

--- show_dirs.py
+++ show_dirs.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import argparse
 
 roots = [
     'sandbox',
@@ -10,6 +11,17 @@ roots = [
     '1',
 ]
 
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--root', default=None)
+    return parser.parse_args()
+
+
+args = get_args()
+print(args)
+roots = [Path('.') / args.root] if args.root else map(
+    lambda root: Path('/workspace') / root, roots)
 for root in roots:
-    for path in (Path('/workspace') / root).glob('*'):
+    for path in root.glob('*'):
         print(path)
