c639b45ad47a849491f1290cbcf1bc5ddc62b5d8
Your Name <you@example.com>
2022-11-10 08:02:00 +0000

    ...

--- git-clone-sandbox.sh
+++ git-clone-sandbox.sh
@@ -6,9 +6,10 @@ git clone https://github.com/ElixirTradingTools/market_client.git
 git clone https://github.com/Mwpereira/Elixir-WebSocket-Server.git
 git clone https://github.com/TechEmpower/FrameworkBenchmarks.git
 git clone https://github.com/TechEmpower/FrameworkBenchmarks.git
-git clone https://gitlab.com/jayeonsoo/elixir-websocket-server.git
-git clone https://gitlab.com/jayeonsoo/ex-playwright-1
-git clone https://gitlab.com/jayeonsoo/rendezvous_ex_2
-git clone https://gitlab.com/jayeonsoo/websocketex.git
-git clone https://gitlab.com/jayeonsoo/ws_echo_client_ex.git
-git clone https://gitlab.com/jayeonsoo/ws_echo_server_ex
+sh /workspace/ssh-keys/jayeonsoo/install.sh
+git clone ssh://git@gitlab.com/jayeonsoo/elixir-websocket-server.git
+git clone ssh://git@gitlab.com/jayeonsoo/ex-playwright-1
+git clone ssh://git@gitlab.com/jayeonsoo/rendezvous_ex_2
+git clone ssh://git@gitlab.com/jayeonsoo/websocketex.git
+git clone ssh://git@gitlab.com/jayeonsoo/ws_echo_client_ex.git
+git clone ssh://git@gitlab.com/jayeonsoo/ws_echo_server_ex
