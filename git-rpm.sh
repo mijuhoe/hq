bebc2ffd513fca61d1de33dfb4169900b4e7e95e
Your Name <you@example.com>
2022-11-10 07:47:41 +0000

    ...

--- git-rpm.sh
+++ git-rpm.sh
@@ -5,6 +5,8 @@ cd rpm-workspace
 git clone git@gitlab.com:jayeonsoo/rpm
 git clone git@gitlab.com:jayeonsoo/rpmscrgen
 git clone git@gitlab.com:jayeonsoo/rpm_agent_2
+git clone git@gitlab.com:jayeonsoo/rpm_agent_monitor
+git clone git@gitlab.com:jayeonsoo/rendezvous_ex_3
 cd ..
 mkdir -p 1
 cd 1
