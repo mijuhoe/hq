edb4d354b5b576e290b8a48a79635c8b5252ba38
Your Name <you@example.com>
2022-11-19 14:51:49 +0000

    ...

---
+++ git-clone-d827088818559.sh
@@ -0,0 +1,7 @@
+cd /workspace
+mkdir -p d827088818559
+cd rpm-workspace
+git clone git@gitlab.com:d827088818559/d1
+git clone git@gitlab.com:d827088818559/rpm-api-server
+git clone git@gitlab.com:d827088818559/rpm-ui
+git clone git@gitlab.com:d827088818559/rpm-work
