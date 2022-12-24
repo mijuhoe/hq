0ef0cdf93a97c2c67f914476679582f6afd74dbe
Your Name <you@example.com>
2022-12-24 01:49:42 +0000

    ...

---
+++ git-clone-bitbucket-jooyool.sh
@@ -0,0 +1,17 @@
+cd /workspace
+mkdir -p 1
+cd 1
+git clone git@bitbucket.org:jooyool/haskellbyexample.git
+git clone git@bitbucket.org:jooyool/haskellsmissingconcurrencybasics.git
+git clone git@bitbucket.org:jooyool/monadexample.git
+git clone git@bitbucket.org:jooyool/stmexample.git
+git clone git@bitbucket.org:jooyool/stmex2.git
+git clone git@bitbucket.org:jooyool/stmex2.git
+git clone git@bitbucket.org:jooyool/rust_by_example_2.git
+git clone git@bitbucket.org:jooyool/cppstlex1.git
+git clone git@bitbucket.org:jooyool/rustbyexample.git
+git clone git@bitbucket.org:jooyool/rust-book.git
+git clone git@bitbucket.org:jooyool/haskellex2.git
+git clone git@bitbucket.org:jooyool/cpp11ex1.git
+git clone git@bitbucket.org:jooyool/haskell-example-1.git
+git clone git@bitbucket.org:jooyool/hython.git
