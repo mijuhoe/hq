778442c6f423e7684994052191078a69d03be40d
Your Name <you@example.com>
2023-01-03 09:05:18 +0000

    ...

--- git-clone-alphasquare.sh
+++ git-clone-alphasquare.sh
@@ -1,6 +1,8 @@
 mkdir -p /workspace/stock
 cd /workspace/stock
+git clone ssh://git@gitlab.com/m8228308850/alphasquare
 git clone ssh://git@gitlab.com/m8228308850/alphasquare_downloader_day
+git clone ssh://git@gitlab.com/m8228308850/alphasquare_downloader_us
 git clone ssh://git@gitlab.com/m8228308850/alphasquare_downloader_crypto
 git clone ssh://git@gitlab.com/m8228308850/alphasquare_downloader_15m
 git clone ssh://git@gitlab.com/m8228308850/alphasquare_downloader_30m
