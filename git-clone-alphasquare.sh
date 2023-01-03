ef416fb115668819995edc13b22de746a48ade68
Your Name <you@example.com>
2023-01-03 09:17:47 +0000

    ...

--- git-clone-alphasquare.sh
+++ git-clone-alphasquare.sh
@@ -4,8 +4,8 @@ git clone ssh://git@gitlab.com/m8228308850/alphasquare
 git clone ssh://git@gitlab.com/m8228308850/alphasquare_downloader_day
 git clone ssh://git@gitlab.com/m8228308850/alphasquare_downloader_us
 git clone ssh://git@gitlab.com/m8228308850/alphasquare_downloader_crypto
-git clone ssh://git@gitlab.com/m8228308850/alphasquare_downloader_15m
-git clone ssh://git@gitlab.com/m8228308850/alphasquare_downloader_30m
-git clone ssh://git@gitlab.com/m8228308850/alphasquare_downloader_60m
-git clone ssh://git@gitlab.com/m8228308850/alphasquare_downloader_120m
 git clone ssh://git@gitlab.com/m8228308850/alphasquare_downloader_240m
+git clone ssh://git@gitlab.com/m8228308850/alphasquare_downloader_120m
+git clone ssh://git@gitlab.com/m8228308850/alphasquare_downloader_60m
+git clone ssh://git@gitlab.com/m8228308850/alphasquare_downloader_30m
+git clone ssh://git@gitlab.com/m8228308850/alphasquare_downloader_15m
