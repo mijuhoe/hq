1bdf902af627971b58805dc5754a11f8ec6c7616
Your Name <you@example.com>
2023-01-26 14:27:09 +0000

    ...

--- git-clone-stock.sh
+++ git-clone-stock.sh
@@ -1,9 +1,13 @@
 mkdir -p /workspace/stock
 cd /workspace/stock
 git clone ssh://git@gitlab.com/m8228308850/backtestV7
-git clone ssh://git@gitlab.com/m8228308850/backtestV6
-git clone ssh://git@gitlab.com/m8228308850/stock_krx
-git clone ssh://git@gitlab.com/m8228308850/stock_naver
-git clone ssh://git@gitlab.com/m8228308850/stock_daum
-git clone ssh://git@gitlab.com/m8228308850/backtestV3
-git clone ssh://git@gitlab.com/m8228308850/backtestV4
+git clone ssh://git@gitlab.com/m8228308850/exbithumb
+git clone ssh://git@gitlab.com/m8228308850/exupbit
+git clone ssh://git@gitlab.com/m8228308850/koreainvestment-autotrade
+git clone ssh://git@gitlab.com/m8228308850/upbit-autotrade
+# git clone ssh://git@gitlab.com/m8228308850/stock_krx
+# git clone ssh://git@gitlab.com/m8228308850/stock_naver
+# git clone ssh://git@gitlab.com/m8228308850/stock_daum
+# git clone ssh://git@gitlab.com/m8228308850/backtestV3
+# git clone ssh://git@gitlab.com/m8228308850/backtestV4
+# git clone ssh://git@gitlab.com/m8228308850/backtestV6
